277b2c539951569971786cdcb567b2c1c76c9357
Douglas C. Rodrigues <107524927+doougg26@users.noreply.github.com>
2023-10-29 16:08:21 -0300

    tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {styled, createGlobalStyle }from 'styled-components';
+import {styled, createGlobalStyle }from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
 *{
@@ -18,7 +18,7 @@ body{
 }
 
 `
-const Container = styled.main`
+const Container = styled.form`
 width:400px;
 height:300px;
 background-color:#fe00fe;
@@ -29,8 +29,7 @@ justify-content: space-around;
 border-radius:10px;
 border:1px solid #000;
 `
-  const Tela = styled.div`
-
+const Tela = styled.div`
 background-color:#00b3fe;
 border:1px solid black;
 border-radius:10px;
@@ -44,12 +43,18 @@ const ContainerTela = styled.section`
 width:350px;
 height:40px;
 display:flex;
-justify-content:space-around;`
+justify-content:space-around;
+text-align:center;`
 
 const Entrada = styled.input`
 width: 320px;
 height:30px;
 border-radius:5px;
+font-weight:bold;
+font-size:20px;
+background-color:#00b3fe;
+border:1px solid black;
+text-align:center;
 `
 const ContainerEntrada = styled.section`
 width:350px;
@@ -63,22 +68,35 @@ width: 320px;
 height:50px;
 align-items: center;
 display: flex;
-justify-content: space-between;
+justify-content: space-evenly;
+background-color:#00b3fe; 
+border:1px solid black;
+border-radius:5px;
 `
 const Botao = styled.button`
 width:30px;
 height:30px;
+font-size: 22px;
 border-radius:50%;
 background-color: #defe47;
+&:hover{
+	cursor: pointer;
+	color:white;
+	background-color:#7700a6; 
+	transform: scale(1.5,1.5);
+	transition: ease-in-out 0.2s;
+}
+`
+const Labels = styled.label`
+color:#000;
+font-size: 20px;
+text-align: center;
 `
 
-function App() {
-	const [primeiroValor, setPrimeiroValor] = useState(0);
-
-	const [segundoValor, setSegundoValor] = useState(0);
-
-	const [resultado, setResultado] = useState(0);
-
+export default function App() {
+	const [primeiroValor, setPrimeiroValor] = useState()
+	const [segundoValor, setSegundoValor] = useState()
+	const [resultado, setResultado] = useState(0)
 
 	const CapturarValor = (item) => {
 		setPrimeiroValor(Number(item.target.value))
@@ -86,24 +104,27 @@ function App() {
 	const CapturarSegundoValor = (item) => {
 		setSegundoValor(Number(item.target.value))
 	}
-
-	const Soma = () => {
+	const Soma = (event) => {
+		event.preventDefault()
 		setResultado(primeiroValor + segundoValor)
 	}
-	const Subtracao = () => {
+	const Subtracao = (event) => {
 		setResultado(primeiroValor - segundoValor)
+		event.preventDefault()
 	}
-	const Divisao = () => {
+	const Divisao = (event) => {
 		setResultado(primeiroValor / segundoValor)
+		event.preventDefault()
 	}
-	const Multiplicacao = () => {
+	const Multiplicacao = (event) => {
 		setResultado(primeiroValor * segundoValor)
+		event.preventDefault()
 	}
-
-	const limpar = () => {
+	const limpar = (event) => {
 		setResultado(0);
-		setPrimeiroValor(0)
-		setSegundoValor(0)
+		setPrimeiroValor("")
+		setSegundoValor("")
+		event.preventDefault()
 	}
 
 	return (
@@ -111,24 +132,39 @@ function App() {
 		<GlobalStyle/>
 			<Container>
 				<h1 style={{color: "#defe47"}}>Calculadora</h1>
-				<ContainerTela>
-					<Tela>{resultado}</Tela><Botao style={{height: "40px", width:"40px"}} onClick={limpar}>C</Botao>
-				</ContainerTela>
 				<ContainerEntrada>
-					<Entrada type="number" onChange={CapturarValor} />
+
+					<Labels>Primeiro Valor
+					<Entrada type="number" value={primeiroValor}onChange={CapturarValor} />
+					</Labels>
+
+					<Labels>Segundo Valor
+					<Entrada type="number" value={segundoValor} onChange={CapturarSegundoValor} />
+					</Labels>
+
+					<Labels>Operaçao</Labels>
+
 					<ContainerBotoes>
 					<Botao onClick={Soma}>+</Botao>
 					<Botao onClick={Subtracao}>-</Botao>
 					<Botao onClick={Divisao}>÷</Botao>
 					<Botao onClick={Multiplicacao}>x</Botao>
 				</ContainerBotoes>
-					<Entrada type="number" onChange={CapturarSegundoValor} />
+
 				</ContainerEntrada>
 
+				<Labels>Resultado</Labels>
+
+				<ContainerTela>
+			
+					<Tela>{resultado.toFixed(1)}</Tela>
+					<Botao style={{height: "40px", width:"40px"}} onClick={limpar}>C</Botao>
+
+				</ContainerTela>
 
 			</Container>
 		</>
 	)
 }
 
-export default App
+
